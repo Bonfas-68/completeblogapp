491fa4fac434199c91d0d80e860e989ec0a0af45
bonfas oluoch <79736339+Bonfas-68@users.noreply.github.com>
2023-05-18 16:52:35 +0300

    Update Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
     <div className="header">
       <div className="logo">Woobly Blogs</div>
       <nav className="navbar">
-        <Link to="/home" className="anchor"> <FaHome className="anchor-icon"/> Home</Link>
+        <Link to="/" className="anchor"> <FaHome className="anchor-icon"/> Home</Link>
           <Link to="/about" className="anchor"><FaInfo className="anchor-icon"/>About</Link>
           <Link to="/blogs" className="anchor"><FaBlog className="anchor-icon"/>Blogs</Link>
       </nav>
