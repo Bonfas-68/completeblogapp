5915ce4d1f4b255b719525c3f30b860b502b7e3a
bonfas oluoch <79736339+Bonfas-68@users.noreply.github.com>
2023-05-18 16:53:26 +0300

    Update App

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
     <BrowserRouter>
     <Header/>
       <Routes>
-        <Route  path='/home' element={<Home/>}/>
+        <Route  path='/' element={<Home/>}/>
         <Route  path='/blogs' element={<Blogs/>}/>
         <Route  path='/about' element={<About/>}/>
         {/* <Route  path='/jokes' element={<Jokes/>}/> */}
